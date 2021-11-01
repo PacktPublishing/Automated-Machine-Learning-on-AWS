c1669cb43fdff731ea7fa0625f9850bfa66fd223
EC2 Default User <ec2-user@ip-172-31-40-174.us-west-2.compute.internal>
2021-11-01 15:55:46 +0000

    Started Chapter 10

---
+++ Chapter10/Files/acme_pipeline_stack.py
@@ -0,0 +1,73 @@
+import aws_cdk.core as cdk
+import aws_cdk.aws_codecommit as codecommit
+import aws_cdk.aws_s3 as s3
+import aws_cdk.pipelines as pipelines
+import aws_cdk.aws_ssm as ssm
+
+class PipelineStack(cdk.Stack):
+
+    def __init__(self, scope: cdk.Construct, id: str, *, model_name: str=None, group_name: str=None, repo_name: str=None, feature_group: str=None, threshold: float=None, cdk_version: str=None, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        self.code_repo = codecommit.Repository(
+            self,
+            "Source-Repository",
+            repository_name=repo_name,
+            description="ACME Web Application Source Code Repository"
+        )
+        cdk.CfnOutput(
+            self,
+            "Clone-URL",
+            description="CodeCommit Clone URL",
+            value=self.code_repo.repository_clone_url_http
+        )
+
+        self.data_bucket = s3.Bucket(
+            self,
+            "Data-Bucket",
+            bucket_name=f"data-{cdk.Aws.REGION}-{cdk.Aws.ACCOUNT_ID}",
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            auto_delete_objects=True,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            versioned=True
+        )
+
+        ssm.StringParameter(
+            self,
+            "Data-Bucket-Parameter",
+            parameter_name="DataBucket",
+            description="SSM Parameter for the S3 Data Bucket Name",
+            string_value=self.data_bucket.bucket_name
+        )
+
+        ssm.StringParameter(
+            self,
+            "Feature-Group-Parameter",
+            parameter_name="FeatureGroup",
+            description="SSM Paramater for the SageMaker Feature Store group",
+            string_value=feature_group
+        )
+
+        source_artifact = pipelines.CodePipelineSource.code_commit(
+            repository=self.code_repo,
+            branch="main"
+        )
+
+        pipeline = pipelines.CodePipeline(
+            self,
+            "Application-Pipeline",
+            pipeline_name="ACME-WebApp-Pipeline",
+            self_mutation=False,
+            cli_version=cdk_version,
+            synth=pipelines.ShellStep(
+                "Synth",
+                input=source_artifact,
+                commands=[
+                    "printenv",
+                    f"npm install -g aws-cdk@{cdk_version}",
+                    "python -m pip install --upgrade pip",
+                    "pip install -r requirements.txt",
+                    "cdk synth"
+                ]
+            )
+        )
---
+++ Chapter10/Files/app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import os
+from aws_cdk import core as cdk
+from acme_web_application.acme_pipeline_stack import PipelineStack
+
+MODEL = "abalone"
+MODEL_GROUP = f"{MODEL.capitalize()}PackageGroup"
+FEATURE_GROUP = "PLACEHOLDER"
+CODECOMMIT_REPOSITORY = "acme-web-application"
+CDK_VERSION = "1.130.0"
+QUALITY_THRESHOLD = 3.1
+
+app = cdk.App()
+
+PipelineStack(
+    app,
+    CODECOMMIT_REPOSITORY,
+    env=cdk.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")),
+    repo_name=CODECOMMIT_REPOSITORY,
+    group_name=MODEL_GROUP,
+    feature_group=FEATURE_GROUP,
+    cdk_version=CDK_VERSION,
+    threshold=QUALITY_THRESHOLD,
+)
+
+app.synth()
