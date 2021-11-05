b14796776292ea3b359df0eb15495e5208f419bd
Trenton Potgieter <darkreapyre@gmail.com>
2021-11-05 20:10:35 +0000

    Updated Continuous Training DAG

--- Chapter10/Files/airflow/dags/continuous_training_pipeline.py
+++ Chapter10/Files/airflow/dags/continuous_training_pipeline.py
@@ -4,7 +4,7 @@ import sagemaker
 import boto3
 import numpy as np
 import pandas as pd
-from time import gmtime, strftime
+from time import sleep
 from datetime import timedelta
 from sagemaker.feature_store.feature_group import FeatureGroup
 
@@ -14,24 +14,72 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.providers.amazon.aws.hooks.lambda_function import AwsLambdaHook
 from airflow.providers.amazon.aws.sensors.s3_prefix import S3PrefixSensor
 
-region_name = "<Add AWS Region>"
+sagemaker_session = sagemaker.Session()
+region_name = sagemaker_session.boto_region_name
 data_bucket = f"""{boto3.client("ssm", region_name=region_name).get_parameter(Name="DataBucket")["Parameter"]["Value"]}"""
+data_prefix = "abalone_data"
 lambda_function = f"""{boto3.client("ssm", region_name=region_name).get_parameter(Name="ReleaseChangeLambda")["Parameter"]["Value"]}"""
 fg_name = f"""{boto3.client("ssm", region_name=region_name).get_parameter(Name="FeatureGroup")["Parameter"]["vlaue"]}"""
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": airflow.utils.dates.days_ago(1),
+    "retries": 0,
+    "retry_delay": timedelta(minutes=2)
+}
 
 
-def start_pipeline(ds, **kwargs):
+def start_pipeline():
     hook = AwsLambdaHook(
         function_name=lambda_function,
         aws_conn_id="aws_default",
         invocation_type="RequestResponse",
-        log_type="None",
+        log_type="Tail",
         qualifier="$LATEST",
         config=None
     )
-    request = hook.invoke_lambda()
+    request = hook.invoke_lambda(payload="null")
     response = json.loads(request["Payload"].read().decode())
-    kwargs["ti"].xcom_push(
-        key="ExecutionId",
-        value=response["ExecutionId"]
+    print(f'ExecutionId: {response["ExecutionId"]}')
+
+
+def update_feature_group():
+    fg = FeatureGroup(name=fg_name, sagemaker_session=sagemaker_session)
+    column_names = ["sex", "length", "diameter", "height", "whole_weight", "shucked_weight", "viscera_weight", "shell_weight", "rings"]
+    abalone_data = pd.read_csv(f"s3://{data_bucket}/{data_prefix}/abalone.new", names=column_names)
+    data = abalone_data[["rings", "sex", "length", "diameter", "height", "whole_weight", "shucked_weight", "viscera_weight", "shell_weight"]]
+    processed_data = pd.get_dummies(data)
+    time_stamp = int(round(time.time()))
+    processed_data["TimeStamp"] = pd.Series([time_stamp] * len(processed_data), dtype="float64")
+    fg.ingest(data_frame=processed_data, max_workers=5, wait=True)
+    sleep(3600)
+
+
+with DAG(
+    dag_id=f"acme-data-workflow",
+    default_args=default_args,
+    schedule_interval="@daily",
+    concurrency=1,
+    max_active_runs=1,
+) as dag:
+
+    s3_trigger = S3PrefixSensor(  
+        task_id="s3_trigger",
+        bucket_name=data_bucket,
+        prefix=data_prefix,
+        dag=dag
     )
+
+    update_fg_task = PythonOperator(
+        task_id="update_fg",
+        python_callable=update_feature_group,
+        dag=dag
+    )
+
+    trigger_release = PythonOperator(
+        task_id="trigger_release_change",
+        python_callable=start_pipeline,
+        dag=dag
+    )
+
+    s3_trigger >> update_feature_group >> trigger_release
