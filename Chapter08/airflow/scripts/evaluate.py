82ef78778b3fbf1087ab95850fa170c479fe7dda
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 03:21:01 +0000

    Adding tarfile member sanitization to extractall()

--- Chapter08/airflow/scripts/evaluate.py
+++ Chapter08/airflow/scripts/evaluate.py
@@ -39,7 +39,26 @@ if __name__ == "__main__":
     model_path = os.path.join(prefix, "model")
     tarfile_path = os.path.join(prefix, "processing/model/model.tar.gz")
     with tarfile.open(tarfile_path) as tar:
-        tar.extractall(path=model_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=model_path)
     print("Loading Trained Model")
     model = load_model(model_path)
     print("Evaluating Trained Model")
--- Chapter10/Files/scripts/evaluation.py
+++ Chapter10/Files/scripts/evaluation.py
@@ -18,7 +18,26 @@ def load_model(base_dir):
     
     model_path = os.path.join(base_dir, "model/model.tar.gz")
     with tarfile.open(model_path) as tar:
-        tar.extractall(".")
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, ".")
     
     model = tf.keras.models.load_model("model.h5")
     model.compile(optimizer="adam", loss="mse")
